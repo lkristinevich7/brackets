8a9a5597d878cc83b88b3c4decc0fd38057eec7d
Liudmila Krystsinevich <lkristinevich.webdev@gmail.com>
2020-03-19 15:59:40 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+// module.exports = function check(str, bracketsConfig) {
+//   var opens = str.match(/\(/\|g) || [],
+//   closes = str.match(/\)/g) || [];
+//   return opens.length === closes.length;
+
+// }
+
+// module.exports = function check(str, bracketsConfig) {
+//   return true
+// }
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // if (bracketsConfig % 2 == 1){
+  //   return false
+  // }
+        
+  count = 0
+  for(let i = 0; i < str.length; i++) {
+      if(str[i] == '(' ){
+        count++
+      }
+          
+      else if(str[i] == ')'){
+        if(count == 0){
+          return false
+        }
+            
+        else{
+          count--
+        }
+      }
+          
+              
+  }
+
+  if(count == 0){
+    return true
+  }
+      
+
+  return false
 }
+
